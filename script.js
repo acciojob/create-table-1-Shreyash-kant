122f3f126c3e81f0f02cda08a7ebfdbbeeb46511
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-03-06 22:31:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function insert_Row() {
   const table = document.getElementById("sampleTable");
 	let tr = document.createElement("tr");
 	let td1 = document.createElement("td");
-	td.textContent = "New Cell1";
+	td1.textContent = "New Cell1";
 	tr.appendChild(td1);
 	let td2 = document.createElement("td");
-	td.textContent = "New Cell2";
+	td2.textContent = "New Cell2";
 	tr.appendChild(td2);
-	table.insertBefore(tr,table.firstElementChild);
+	table.insertBefore(tr,table.rows[0]);
   
 }
